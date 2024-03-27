e7a25241bb4dc424338bf73ce195520e27f8adf1
vxittao <v-xittao@microsoft.com>
2024-03-27 09:47:34 +0000

    11

---
+++ TestProject2/UnitTest1.cs
@@ -0,0 +1,133 @@
+namespace TestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 1;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int a = 1;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            int a = 1;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            int a = 1;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod12()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod13()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod14()
+        {
+            int a = 1;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod15()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod16()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod17()
+        {
+            int a = 1;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod18()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod19()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod20()
+        {
+            int a = 2;
+            Console.WriteLine(a);
+        }
+        [TestMethod]
+        public void TestMethod21()
+        {
+            int a = 2;
+            Assert.AreEqual(2, a);
+        }
+    }
+}
